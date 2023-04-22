c5c133e7675b783c751d321536edc15150c667df
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-04-22 17:18:43 +0300

    Finished the new task screen.

--- screens/NewTaskScreen.js
+++ screens/NewTaskScreen.js
@@ -1,3 +1,33 @@
 import React from "react";
 import { View } from "react-native";
 import { TextInput, Button, useTheme } from "react-native-paper";
+
+export default function NewTaskScreen({ route, navigation }) {
+  const theme = useTheme();
+  const { projectId } = route.params;
+  const [taskName, setTaskName] = React.useState("");
+
+  function handleSave() {
+    // TODO: save the task to the database after you implement it.
+    console.log(`Saved task ${taskName} to project ${projectId}`);
+    navigation.goBack();
+  }
+
+  return (
+    <View style={{ padding: 16 }}>
+      <TextInput
+        label="Task Name"
+        value={taskName}
+        onChangeText={setTaskName}
+        style={{ marginBottom: 16 }}
+      />
+      <Button
+        mode="contained"
+        onPress={handleSave}
+        color={theme.colors.primary}
+      >
+        Save
+      </Button>
+    </View>
+  );
+}
