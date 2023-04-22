82b322b3fbd1ed3a3fb40ba8cde8f97883fbb51a
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-04-22 17:36:53 +0300

    Displaying home screen on App, errors showing up. Will fix.

--- App.js
+++ App.js
@@ -1,31 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
+import HomeScreen from "./screens/HomeScreen";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Changes you make will automatically reload.</Text>
-      <Text>Shake your phone to open the developer menu.</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <HomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    fontSize: 30,
-  },
-  // create a dark-mode class
-  dark: {
-    flex: 1,
-    backgroundColor: "#000",
-    color: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
