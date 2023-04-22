efd2a4f4bda8872e16cb409830ba367e2f928e20
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-04-22 20:18:29 +0300

    Updated new tasks screen. Error persists. Will look at home screen and the new project screen.

--- screens/NewTaskScreen.js
+++ screens/NewTaskScreen.js
@@ -1,16 +1,40 @@
 import React from "react";
 import { View } from "react-native";
 import { TextInput, Button, useTheme } from "react-native-paper";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function NewTaskScreen({ route, navigation }) {
   const theme = useTheme();
-  const { projectId } = route.params;
+  const { projectId, name } = route.params;
   const [taskName, setTaskName] = React.useState("");
 
-  function handleSave() {
-    // TODO: save the task to the database after you implement it.
-    console.log(`Saved task ${taskName} to project ${projectId}`);
-    navigation.goBack();
+  async function handleSave() {
+    if (taskName === "") {
+      return;
+    }
+    try {
+      const taskId = Date.now().toString();
+      const task = {
+        id: taskId,
+        name: taskName,
+        completed: false,
+      };
+      const tasksJson = await AsyncStorage.getItem(projectId.toString());
+      if (tasksJson != null) {
+        const tasks = JSON.parse(tasksJson);
+        tasks.push(task);
+        await AsyncStorage.setItem(projectId.toString(), JSON.stringify(tasks));
+      } else {
+        await AsyncStorage.setItem(
+          projectId.toString(),
+          JSON.stringify([task])
+        );
+      }
+      console.log(`New task ${task.name} saved!`);
+      navigation.goBack();
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   return (
