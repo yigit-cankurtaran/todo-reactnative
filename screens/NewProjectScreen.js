c212c72f1e68b104833ea5be99d5a93937e01701
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-04-22 20:04:02 +0300

    Updated NewProjectScreen to use AsyncStorage, nothing has changed.

--- screens/NewProjectScreen.js
+++ screens/NewProjectScreen.js
@@ -1,15 +1,28 @@
 import React from "react";
 import { View } from "react-native";
 import { TextInput, Button, useTheme } from "react-native-paper";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function NewProjectScreen({ navigation }) {
   const theme = useTheme();
   const [projectName, setProjectName] = React.useState("");
 
-  function handleSave() {
-    // TODO: save the project to the database after you implement it.
-    console.log(`Saved project ${projectName}`);
-    navigation.goBack();
+  async function handleSave() {
+    try {
+      const NewProject = {
+        id: new Date().getTime(),
+        name: projectName,
+        tasks: [],
+      };
+      await AsyncStorage.setItem(
+        NewProject.id.toString(),
+        JSON.stringify(NewProject)
+      );
+      console.log(`New project ${NewProject.name} saved!`);
+      navigation.goBack();
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   return (
