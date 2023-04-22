680a7e8e17319a446f67a2f737241f3fcf3ac4d8
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-04-22 16:42:50 +0300

    Played around a bit, installed some new dependencies.

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Changes you make will automatically reload.</Text>
+      <Text>Shake your phone to open the developer menu.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 30,
+  },
+  // create a dark-mode class
+  dark: {
+    flex: 1,
+    backgroundColor: "#000",
+    color: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
