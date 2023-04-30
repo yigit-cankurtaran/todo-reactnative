85c6ffa6f710973279e39e993bb5d5083e889094
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-04-30 15:46:12 +0300

    Started changing project screen to add tasks.

--- screens/ProjectScreen.js
+++ screens/ProjectScreen.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { View, FlatList } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
 import { List, Divider, FAB, useTheme } from "react-native-paper";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import NewTasksScreen from "./NewTaskScreen";
 
 export default function ProjectScreen({ route, navigation }) {
   const theme = useTheme();
