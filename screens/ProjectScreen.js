a480f49b387c2d63bc710050e1f51f979532251f
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-05-01 21:54:55 +0300

    Changed the ProjectScreen around. Haven't tested yet.

--- screens/ProjectScreen.js
+++ screens/ProjectScreen.js
@@ -1,29 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { View, FlatList, StyleSheet } from "react-native";
-import { List, Divider, FAB, useTheme } from "react-native-paper";
+import { View, FlatList, Alert } from "react-native";
+import { List, FAB, useTheme } from "react-native-paper";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import NewTaskScreen from "./NewTaskScreen";
 
 export default function ProjectScreen({ route, navigation }) {
-  const theme = useTheme();
   const { id, name } = route.params;
   const [tasks, setTasks] = useState([]);
+  const theme = useTheme();
 
-  console.log(route.params);
-
+  // Fetching tasks from AsyncStorage when the component mounts
   useEffect(() => {
     async function fetchTasks() {
       try {
-        const tasksJson = await AsyncStorage.getItem(id.toString());
-        if (tasksJson != null) {
-          setTasks(JSON.parse(tasksJson));
+        const jsonValue = await AsyncStorage.getItem(`@tasks_${id}`);
+        if (jsonValue != null) {
+          setTasks(JSON.parse(jsonValue));
         }
       } catch (e) {
         console.log(e);
       }
     }
     fetchTasks();
-  }, [id]);
+  }, []);
+
+  // Saving tasks to AsyncStorage when the tasks state changes
+  useEffect(() => {
+    async function saveTasks() {
+      try {
+        const jsonValue = JSON.stringify(tasks);
+        await AsyncStorage.setItem(`@tasks_${id}`, jsonValue);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    saveTasks();
+  }, [tasks]);
 
   function handleAddTask() {
     console.log(id);
@@ -36,38 +47,43 @@ export default function ProjectScreen({ route, navigation }) {
     });
   }
 
-  async function handleDeleteTask(taskId) {
-    try {
-      const updatedTasks = tasks.filter((task) => task.id !== taskId);
-      await AsyncStorage.setItem(id.toString(), JSON.stringify(updatedTasks));
-      setTasks(updatedTasks);
-      console.log(`Task ${taskId} deleted`);
-      if (updatedTasks.length === 0) {
-        await AsyncStorage.removeItem(id.toString());
-        navigation.goBack();
-        console.log(`Project ${id} deleted`);
-      }
-      // did not fix
-    } catch (e) {
-      console.log(e);
-    }
+  async function handleToggleTask(taskId) {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, done: !task.done } : task
+    );
+    setTasks(updatedTasks);
   }
 
-  async function handleToggleTask(taskId) {
-    try {
-      const index = tasks.findIndex((task) => task.id === taskId);
-      if (index === -1) {
-        console.log(`No task with id ${taskId}`);
-        return;
-      }
-      const updatedTasks = [...tasks];
-      updatedTasks[index] = { ...tasks[index], done: !tasks[index].done };
-      await AsyncStorage.setItem(id.toString(), JSON.stringify(updatedTasks));
-      setTasks(updatedTasks);
-      console.log(`Task ${taskId} toggled`);
-    } catch (e) {
-      console.log(e);
-    }
+  async function handleDeleteTask(taskId) {
+    Alert.alert(
+      "Delete Task",
+      "Are you sure you want to delete this task?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Delete",
+          onPress: () => {
+            // Remove task from AsyncStorage
+            async function removeTask() {
+              try {
+                await AsyncStorage.removeItem(`@task_${taskId}`);
+              } catch (e) {
+                console.error(e);
+              }
+            }
+            removeTask();
+
+            // Remove task from tasks state
+            const filteredTasks = tasks.filter((task) => task.id !== taskId);
+            setTasks(filteredTasks);
+          },
+        },
+      ],
+      { cancelable: false }
+    );
   }
 
   function renderTaskItem({ item, index }) {
