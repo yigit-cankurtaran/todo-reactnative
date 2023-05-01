693b3646341816b1dc78e8d8979e3a6b5be32d79
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-05-01 19:26:19 +0300

    The error I'm getting changed, nothing deletes, textinput updates properly. Will fix.

--- screens/NewTaskScreen.js
+++ screens/NewTaskScreen.js
@@ -1,66 +1,55 @@
-import React from "react";
-import { View, TextInput, TouchableOpacity, Text } from "react-native";
-import { useTheme } from "react-native-paper";
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+} from "react-native";
+import { Button, useTheme } from "react-native-paper";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function NewTaskScreen({ route, navigation }) {
   const theme = useTheme();
-  const { projectId, name } = route.params;
-  const [taskName, setTaskName] = React.useState("");
+  const { projectId, taskId, tasks } = route.params;
+  const [taskName, setTaskName] = useState("");
 
   async function handleSave() {
-    console.log(`The first: ${taskName}`);
-    console.log(`The second: ${name}`);
-    console.log(`The third: ${projectId}`);
-
-    if (taskName === "") {
-      return;
-    }
+    if (taskName.trim() === "") return;
     try {
-      const taskId = Date.now().toString();
-      console.log(`The fourth: ${taskId}`);
-      const task = {
-        id: taskId,
-        name: taskName,
-        completed: false,
-      };
-      let tasks = [];
-      const tasksJson = await AsyncStorage.getItem(projectId.toString());
-      console.log(`The fifth: ${tasksJson}`);
-      if (tasksJson != null) {
-        const parsedTasks = JSON.parse(tasksJson);
-        console.log(`The sixth: ${parsedTasks}`);
-        tasks = Array.isArray(parsedTasks) ? parsedTasks : [parsedTasks];
-        console.log(`The seventh: ${tasks}`);
-      }
-      tasks.push(task);
-      console.log(`The eighth: ${task}`);
-      await AsyncStorage.setItem(projectId.toString(), JSON.stringify(tasks));
-      console.log(`New task ${task.name} saved!`);
+      const newTask = { id: taskId, name: taskName, done: false };
+      const updatedTasks = [...tasks, newTask];
+      await AsyncStorage.setItem(
+        projectId.toString(),
+        JSON.stringify(updatedTasks)
+      );
       navigation.goBack();
+      console.log(`Task ${taskId} saved`);
     } catch (e) {
       console.log(e);
     }
   }
 
   return (
-    <View style={{ padding: 16 }}>
+    <View style={styles.container}>
       <TextInput
         label="Task Name"
         value={taskName}
         onChangeText={setTaskName}
-        style={{ marginBottom: 16 }}
       />
-      <TouchableOpacity
-        onPress={handleSave}
-        style={{
-          backgroundColor: theme.colors.primary,
-          padding: 12,
-          borderRadius: 5,
-        }}
-      >
-        <Text style={{ color: "#FFF", textAlign: "center" }}>Save</Text>
-      </TouchableOpacity>
+      <Button mode="contained" onPress={handleSave} style={styles.button}>
+        Save
+      </Button>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  button: {
+    alignSelf: "center",
+  },
+});
