617abfd334641edff81203dadbbd619b469d5ea5
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-05-02 13:13:01 +0300

    Finished writing the new NewTaskScreen

--- screens/NewTaskScreen.js
+++ screens/NewTaskScreen.js
@@ -1,50 +1,41 @@
 import React, { useState } from "react";
-import {
-  View,
-  TextInput,
-  TouchableOpacity,
-  Text,
-  StyleSheet,
-} from "react-native";
-import { Button, useTheme } from "react-native-paper";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation, useRoute } from "@react-navigation/native";
 
-export default function NewTaskScreen({ route, navigation }) {
-  const theme = useTheme();
-  const { projectId, taskId, tasks } = route.params;
+export default function NewTaskScreen() {
   const [taskName, setTaskName] = useState("");
+  const navigation = useNavigation();
+  const route = useRoute();
+  const project = route.params.project; // get the project data from the route
 
-  async function handleSave() {
-    if (taskName.trim() === "") return;
-    try {
-      const newTask = { id: taskId, name: taskName, done: false };
-      const updatedTasks = Array.isArray(tasks)
-        ? [...tasks, newTask]
-        : [newTask];
-      // this part breaks my code.
-
-      await AsyncStorage.setItem(
-        projectId.toString(),
-        JSON.stringify(updatedTasks)
-      );
-      navigation.goBack();
-      console.log(`Task ${taskId} saved`);
-    } catch (e) {
-      console.log(e);
-    }
+  function saveTask() {
+    // create new task
+    const newTask = {
+      id: Math.random().toString(),
+      name: taskName,
+      completed: false,
+    };
+    // update project data with new task
+    const updatedProject = {
+      ...project,
+      tasks: [...project.tasks, newTask],
+    };
+    // set updated project data as parameter and navigate back to ProjectScreen
+    navigation.setParams({ project: updatedProject });
+    navigation.goBack();
   }
 
   return (
     <View style={styles.container}>
+      <Text>New Task</Text>
       <TextInput
-        label="Task Name"
+        placeholder="Enter task name."
         value={taskName}
-        onChangeText={setTaskName}
+        onChangeText={(text) => setTaskName(text)}
       />
-      <Button mode="contained" onPress={handleSave} style={styles.button}>
-        Save
-      </Button>
+      <Button title="Save" onPress={saveTask} style={styles.button} />
     </View>
   );
 }
