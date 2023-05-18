354c2badd61c8b818ee20a890289bcf456b947fa
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-05-18 20:23:34 +0300

    Removed some comments from App.jsx, re-building app.

--- App.jsx
+++ App.jsx
@@ -1,14 +1,5 @@
 import Navigation from "./Navigation";
-// app crashes, we need to import more stuff
 import React from "react";
-// app crashes, we need to import more stuff
-// import { NavigationContainer } from "@react-navigation/native";
-// import { createStackNavigator } from "@react-navigation/stack";
-
-// // import the screens
-// import HomeScreen from "../screens/HomeScreen";
-// import DetailScreen from "../screens/DetailScreen";
-// import AddScreen from "../screens/AddScreen";
 
 export default function App() {
   return <Navigation />;
