4deff1d9aefa4af76276ec4d3b0679fc7ea810b3
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-05-18 18:02:18 +0300

    Imported some libs on the App.jsx file. Trying to fix app crashing.

--- App.jsx
+++ App.jsx
@@ -1,4 +1,14 @@
 import Navigation from "./Navigation";
+// app crashes, we need to import more stuff
+import React from "react";
+// app crashes, we need to import more stuff
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
+
+// // import the screens
+// import HomeScreen from "../screens/HomeScreen";
+// import DetailScreen from "../screens/DetailScreen";
+// import AddScreen from "../screens/AddScreen";
 
 export default function App() {
   return <Navigation />;
