4b9cf8af8ee0caad15295a8b4bdf2a6cec344b9c
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-05-25 13:11:13 +0300

    Implementing dark theme. Added a switch and a theme to the navigation file.

--- Navigation.jsx
+++ Navigation.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { useColorScheme, Switch } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
 import HomeScreen from "./screens/HomeScreen";
 import ProjectScreen from "./screens/ProjectScreen";
 import NewProjectScreen from "./screens/NewProjectScreen";
@@ -9,14 +14,35 @@ import NewTaskScreen from "./screens/NewTaskScreen";
 const Stack = createStackNavigator();
 
 export default function Navigation() {
+  // Detect the device theme
+  const scheme = useColorScheme();
+  // Create a state variable to store the theme value
+  const [isDarkTheme, setIsDarkTheme] = useState(scheme === "dark");
+  // Create a function to toggle the theme value
+  const toggleTheme = () => {
+    setIsDarkTheme((prev) => !prev);
+  };
+
   return (
-    <NavigationContainer>
+    // Pass the theme value to the NavigationContainer component
+    <NavigationContainer theme={isDarkTheme ? DarkTheme : DefaultTheme}>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* Pass the theme value as a prop to your HomeScreen component */}
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          initialParams={{ theme: isDarkTheme ? DarkTheme : DefaultTheme }}
+        />
         <Stack.Screen name="Project" component={ProjectScreen} />
         <Stack.Screen name="New Project" component={NewProjectScreen} />
         <Stack.Screen name="New Task" component={NewTaskScreen} />
       </Stack.Navigator>
+      {/* Add a switch component to toggle the theme */}
+      <Switch
+        style={{ position: "absolute", margin: 16, left: 0, bottom: 0 }}
+        value={isDarkTheme}
+        onValueChange={toggleTheme}
+      />
     </NavigationContainer>
   );
 }
